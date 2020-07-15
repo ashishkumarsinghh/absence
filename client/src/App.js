9e39dcc32df987f1ee3980e561666c861a6f36ae
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-07-15 22:54:14 +0530

    initialize nestjs and react

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import config from "./config";
+function App() {
+  useEffect(() => {
+    fetch(`${config.apiServerBaseUrl}api`).then((res) =>
+      res.json().then((json) => console.log(json))
+    );
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/config.js
@@ -0,0 +1,4 @@
+const config = {
+  apiServerBaseUrl: "http://localhost:3001/",
+};
+export default config;
